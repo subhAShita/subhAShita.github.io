8a5bd79d0ca3b83c8cd3795e514277d79929f0ca
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2022-03-19 21:29:15 +0530

    CU

--- static/articles/wsc2022/build.sh
+++ static/articles/wsc2022/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
+
+while true; do
 rm *.bbl *.blg *.aux
 xelatex subhAShita-db-deduplication
 bibtex subhAShita-db-deduplication
 xelatex subhAShita-db-deduplication
 xelatex subhAShita-db-deduplication
-
+sleep 25
+done
